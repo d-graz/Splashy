58256dc34825da738fc54e73a6906e2fafa9a8cc
Davide Grazzani <davide.grazzani@mail.polimi.it>
2024-04-24 17:05:19 +0200

    Added new filter for Touch sensor

--- src/Arduino/Splashy/libraries/TouchSensor/TouchSensor.cpp
+++ src/Arduino/Splashy/libraries/TouchSensor/TouchSensor.cpp
@@ -4,6 +4,9 @@ TouchSensor::TouchSensor(const char* name): Task(name), sensor(TOUCH_SENSOR_PIN_
     this->touch = false;
     // FIXME: [LOW] autocalibration should be used for production-ready code
     this->sensor.set_CS_AutocaL_Millis(0xFFFFFFFF);
+    for (byte i = 0; i < FILTER_SIZE_TS; i++) {
+        this->samples[i] = this->sensor.capacitiveSensor(SAMPLES);
+    }
 }
 
 bool TouchSensor::is_active() {
@@ -19,16 +22,27 @@ bool TouchSensor::next() {
             }
         #endif    
     #endif
-    long sensor_value = this->sensor.capacitiveSensor(SAMPLES);
-    #ifdef TOUCH_SENSOR_DEBUG
-        Serial.print(F("Sensor value: "));
-        Serial.println(sensor_value);
-    #endif
-    if(sensor_value > THRESHOLD){
+    for(byte i = 0; i < FILTER_UPDATE_SIZE_TS; i++){
+        this->samples[this->index] = this->sensor.capacitiveSensor(SAMPLES);
+        #ifdef TOUCH_SENSOR_DEBUG
+            Serial.print(F("Touch Sensor value: "));
+            Serial.println(this->samples[this->index]);
+        #endif
+        this->index = (this->index + 1) % FILTER_SIZE_TS;
+    }
+    if(this->get_filtered_value() > THRESHOLD){
         this->touch = true;
     } else {
         this->touch = false;
     }
     this->update_next_execution_millis();
     return true;
+}
+
+float TouchSensor::get_filtered_value(){
+    float sum = 0;
+    for (byte i = 0; i < FILTER_SIZE_TS; i++) {
+        sum += this->samples[i];
+    }
+    return sum / FILTER_SIZE_TS;
 }
--- src/Arduino/Splashy/libraries/TouchSensor/TouchSensor.hpp
+++ src/Arduino/Splashy/libraries/TouchSensor/TouchSensor.hpp
@@ -1,3 +1,8 @@
+/**
+ * @file TouchSensor.hpp
+ * @brief This file contains the declaration of the TouchSensor class.
+ * @version 1.1
+*/
 #ifndef __TOUCH_SENSOR__
 #define __TOUCH_SENSOR__
 
@@ -9,18 +14,40 @@
 #define TOUCH_SENSOR_DEBUG
 #endif
 
+//TODO: [TEST] scoprire la causa delle oscillazioni + test del nuovo filtro
 #define SAMPLES 1500 ///< The number of samples to take when reading the touch sensor.
-#define THRESHOLD 75 ///< The threshold value for the touch sensor to be recognized as active.
+#define THRESHOLD 200 ///< The threshold value for the touch sensor to be recognized as active.
 #define SLEEP_TIME 200 ///< The time to sleep between samples in milliseconds.
+#define FILTER_SIZE_TS 4 ///< The size of the filter to use when smoothing the sensor readings.
+#define FILTER_UPDATE_SIZE_TS 2 ///< The number of samples to use when updating the filter.
 
+/**
+ * @class TouchSensor
+ * @brief This class represents a touch sensor.
+ * 
+ * The TouchSensor class extends the Task class and provides methods for checking if the touch sensor is active.
+ */
 class TouchSensor: public Task {
     private:
         CapacitiveSensor sensor; ///< The capacitive sensor object.
+        unsigned int samples[SAMPLES]; ///< The array of samples taken from the sensor.
+        byte index; ///< The current index in the samples array.
         bool touch; ///< A flag to indicate whether the touch sensor is currently active.
+
+        /**
+         * @brief Get the filtered value of the sensor.
+         * 
+         * This method calculates the filtered value of the sensor by taking the average of the samples in the filter.
+         * 
+         * @return The filtered value of the sensor.
+         */
+        float get_filtered_value();
 
     public:
         /**
          * @brief Construct a new TouchSensor object.
+         * 
+         * This constructor creates a new TouchSensor object with the given name.
          * 
          * @param name The name of the task.
          */
@@ -29,7 +56,7 @@ class TouchSensor: public Task {
         /**
          * @brief Perform the next step in the task.
          * 
-         * This method should be implemented to perform the next step in the task.
+         * This method should be implemented to perform the next step in the task. It is called periodically by the task manager.
          * 
          * @return true if the task is complete, false otherwise.
          */
@@ -37,6 +64,8 @@ class TouchSensor: public Task {
 
         /**
          * @brief Check if the touch sensor is active.
+         * 
+         * This method checks if the touch sensor is currently active. It does this by reading the value from the capacitive sensor and comparing it to a threshold.
          * 
          * @return true if the touch sensor is active, false otherwise.
          */
