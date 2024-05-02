9386a4b59765966b4a5a16adf565ef8532a5676e
giallumigliet <163427589+giallumigliet@users.noreply.github.com>
2024-05-02 13:03:43 +0200

    Add files via upload

--- src/Raspberry/GUIcode.py
+++ src/Raspberry/GUIcode.py
@@ -5,67 +5,39 @@ import sys
 from PySide6 import QtCore, QtGui, QtWidgets
 from PySide6.QtWidgets import QTableWidgetItem
 
+from PySide6.QtCore import (QCoreApplication, QPropertyAnimation, QDate, QDateTime, QMetaObject, QObject, QPoint, QRect, QSize, QTime, QUrl, Qt, QEvent)
+from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor, QFont, QFontDatabase, QIcon, QKeySequence, QLinearGradient, QPalette, QPainter, QPixmap, QRadialGradient, QPen)
+from PySide6.QtWidgets import *
+  
+#IMPORT PYSIDE2EXTN WIDGET YOU USED IN THE QTDESIGNER FOR DESIGNING.
+from PySide2extn.RoundProgressBar import roundProgressBar
+
 #python file GUIsplashy.py generated from GUIsplashy.ui 
 #USING COMMAND:  
 #   cd /home/gianluca/Desktop/splashy/python_env
 #   pyuic5 GUIsplashy.ui  -o GUIsplashy.py
 ##########################################################################################################
+
+
 class Ui_MainWindowSplashy(object):
     def setupUi(self, MainWindowSplashy):
         MainWindowSplashy.setObjectName("MainWindowSplashy")
         MainWindowSplashy.resize(800, 406)
         MainWindowSplashy.setStyleSheet("background-color: qlineargradient(spread:pad, x1:0, y1:0, x2:0.637368, y2:0.943, stop:0 rgba(127, 163, 216, 255), stop:0.989637 rgba(236, 245, 255, 255))")
         self.centralwidget = QtWidgets.QWidget(MainWindowSplashy)
+        self.centralwidget.setStyleSheet("background: rgb(85, 87, 83)")
         self.centralwidget.setObjectName("centralwidget")
-        self.tableRanking = QtWidgets.QTableWidget(self.centralwidget)
-        self.tableRanking.setGeometry(QtCore.QRect(20, 50, 351, 121))
-        self.tableRanking.setObjectName("tableRanking")
-        self.tableRanking.setColumnCount(3)
-        self.tableRanking.setRowCount(3)
-        item = QtWidgets.QTableWidgetItem()
-        self.tableRanking.setVerticalHeaderItem(0, item)
-        item = QtWidgets.QTableWidgetItem()
-        self.tableRanking.setVerticalHeaderItem(1, item)
-        item = QtWidgets.QTableWidgetItem()
-        self.tableRanking.setVerticalHeaderItem(2, item)
-        item = QtWidgets.QTableWidgetItem()
-        self.tableRanking.setHorizontalHeaderItem(0, item)
-        item = QtWidgets.QTableWidgetItem()
-        self.tableRanking.setHorizontalHeaderItem(1, item)
-        item = QtWidgets.QTableWidgetItem()
-        self.tableRanking.setHorizontalHeaderItem(2, item)
-        self.tableCurrentUser = QtWidgets.QTableWidget(self.centralwidget)
-        self.tableCurrentUser.setGeometry(QtCore.QRect(120, 240, 501, 51))
-        self.tableCurrentUser.setObjectName("tableCurrentUser")
-        self.tableCurrentUser.setColumnCount(4)
-        self.tableCurrentUser.setRowCount(1)
-        item = QtWidgets.QTableWidgetItem()
-        self.tableCurrentUser.setVerticalHeaderItem(0, item)
-        item = QtWidgets.QTableWidgetItem()
-        self.tableCurrentUser.setHorizontalHeaderItem(0, item)
-        item = QtWidgets.QTableWidgetItem()
-        self.tableCurrentUser.setHorizontalHeaderItem(1, item)
-        item = QtWidgets.QTableWidgetItem()
-        self.tableCurrentUser.setHorizontalHeaderItem(2, item)
-        item = QtWidgets.QTableWidgetItem()
-        self.tableCurrentUser.setHorizontalHeaderItem(3, item)
-        item = QtWidgets.QTableWidgetItem()
-        self.tableCurrentUser.setItem(0, 0, item)
         self.label = QtWidgets.QLabel(self.centralwidget)
         self.label.setGeometry(QtCore.QRect(20, 20, 211, 17))
         self.label.setStyleSheet("background: transparent")
         self.label.setObjectName("label")
-        self.label_2 = QtWidgets.QLabel(self.centralwidget)
-        self.label_2.setGeometry(QtCore.QRect(490, 30, 111, 17))
-        self.label_2.setStyleSheet("background: transparent")
-        self.label_2.setObjectName("label_2")
         self.label_3 = QtWidgets.QLabel(self.centralwidget)
-        self.label_3.setGeometry(QtCore.QRect(190, 210, 161, 17))
+        self.label_3.setGeometry(QtCore.QRect(450, 320, 231, 31))
         self.label_3.setAutoFillBackground(False)
-        self.label_3.setStyleSheet("background: transparent")
+        self.label_3.setStyleSheet("color: rgb(255, 255, 255); background: transparent")
         self.label_3.setObjectName("label_3")
         self.layoutWidget = QtWidgets.QWidget(self.centralwidget)
-        self.layoutWidget.setGeometry(QtCore.QRect(480, 110, 260, 27))
+        self.layoutWidget.setGeometry(QtCore.QRect(390, 80, 283, 27))
         self.layoutWidget.setObjectName("layoutWidget")
         self.horizontalLayout = QtWidgets.QHBoxLayout(self.layoutWidget)
         self.horizontalLayout.setContentsMargins(0, 0, 0, 0)
@@ -83,19 +55,67 @@ class Ui_MainWindowSplashy(object):
         self.labelSplashyWeeklyGoal.setStyleSheet("background: transparent")
         self.labelSplashyWeeklyGoal.setObjectName("labelSplashyWeeklyGoal")
         self.horizontalLayout.addWidget(self.labelSplashyWeeklyGoal)
-        self.layoutWidget1 = QtWidgets.QWidget(self.centralwidget)
-        self.layoutWidget1.setGeometry(QtCore.QRect(480, 70, 172, 19))
-        self.layoutWidget1.setObjectName("layoutWidget1")
-        self.horizontalLayout_2 = QtWidgets.QHBoxLayout(self.layoutWidget1)
-        self.horizontalLayout_2.setContentsMargins(0, 0, 0, 0)
-        self.horizontalLayout_2.setObjectName("horizontalLayout_2")
-        self.label_6 = QtWidgets.QLabel(self.layoutWidget1)
-        self.label_6.setObjectName("label_6")
-        self.horizontalLayout_2.addWidget(self.label_6)
-        self.labelPlasticSaved = QtWidgets.QLabel(self.layoutWidget1)
+        self.label_5 = QtWidgets.QLabel(self.centralwidget)
+        self.label_5.setGeometry(QtCore.QRect(450, 350, 231, 31))
+        self.label_5.setAutoFillBackground(False)
+        self.label_5.setStyleSheet("color: rgb(255, 255, 255); background: transparent")
+        self.label_5.setObjectName("label_5")
+        self.labelPlasticSaved = QtWidgets.QLabel(self.centralwidget)
+        self.labelPlasticSaved.setGeometry(QtCore.QRect(390, 140, 170, 17))
         self.labelPlasticSaved.setStyleSheet("background: transparent")
         self.labelPlasticSaved.setObjectName("labelPlasticSaved")
-        self.horizontalLayout_2.addWidget(self.labelPlasticSaved)
+        self.horizontalLayoutWidget = QtWidgets.QWidget(self.centralwidget)
+        self.horizontalLayoutWidget.setGeometry(QtCore.QRect(30, 60, 301, 230))
+        self.horizontalLayoutWidget.setObjectName("horizontalLayoutWidget")
+        self.horizontalLayout_2 = QtWidgets.QHBoxLayout(self.horizontalLayoutWidget)
+        self.horizontalLayout_2.setContentsMargins(0, 0, 0, 0)
+        self.horizontalLayout_2.setObjectName("horizontalLayout_2")
+        self.verticalLayout = QtWidgets.QVBoxLayout()
+        self.verticalLayout.setObjectName("verticalLayout")
+        self.label_nickname1 = QtWidgets.QLabel(self.horizontalLayoutWidget)
+        self.label_nickname1.setStyleSheet("background: transparent")
+        self.label_nickname1.setObjectName("label_nickname1")
+        self.verticalLayout.addWidget(self.label_nickname1)
+        self.label_nickname2 = QtWidgets.QLabel(self.horizontalLayoutWidget)
+        self.label_nickname2.setStyleSheet("background: transparent")
+        self.label_nickname2.setObjectName("label_nickname2")
+        self.verticalLayout.addWidget(self.label_nickname2)
+        self.label_nickname3 = QtWidgets.QLabel(self.horizontalLayoutWidget)
+        self.label_nickname3.setStyleSheet("background: transparent")
+        self.label_nickname3.setObjectName("label_nickname3")
+        self.verticalLayout.addWidget(self.label_nickname3)
+        self.label_nickname4 = QtWidgets.QLabel(self.horizontalLayoutWidget)
+        self.label_nickname4.setStyleSheet("background: transparent")
+        self.label_nickname4.setObjectName("label_nickname4")
+        self.verticalLayout.addWidget(self.label_nickname4)
+        self.label_nickname5 = QtWidgets.QLabel(self.horizontalLayoutWidget)
+        self.label_nickname5.setStyleSheet("background: transparent")
+        self.label_nickname5.setObjectName("label_nickname5")
+        self.verticalLayout.addWidget(self.label_nickname5)
+        self.horizontalLayout_2.addLayout(self.verticalLayout)
+        self.verticalLayout_2 = QtWidgets.QVBoxLayout()
+        self.verticalLayout_2.setObjectName("verticalLayout_2")
+        self.label_literRanking1 = QtWidgets.QLabel(self.horizontalLayoutWidget)
+        self.label_literRanking1.setStyleSheet("background: transparent")
+        self.label_literRanking1.setObjectName("label_literRanking1")
+        self.verticalLayout_2.addWidget(self.label_literRanking1)
+        self.label_literRanking2 = QtWidgets.QLabel(self.horizontalLayoutWidget)
+        self.label_literRanking2.setStyleSheet("background: transparent")
+        self.label_literRanking2.setObjectName("label_literRanking2")
+        self.verticalLayout_2.addWidget(self.label_literRanking2)
+        self.label_literRanking3 = QtWidgets.QLabel(self.horizontalLayoutWidget)
+        self.label_literRanking3.setStyleSheet("background: transparent")
+        self.label_literRanking3.setObjectName("label_literRanking3")
+        self.verticalLayout_2.addWidget(self.label_literRanking3)
+        self.label_literRanking4 = QtWidgets.QLabel(self.horizontalLayoutWidget)
+        self.label_literRanking4.setStyleSheet("background: transparent")
+        self.label_literRanking4.setObjectName("label_literRanking4")
+        self.verticalLayout_2.addWidget(self.label_literRanking4)
+        self.label_literRanking5 = QtWidgets.QLabel(self.horizontalLayoutWidget)
+        self.label_literRanking5.setStyleSheet("background: transparent")
+        self.label_literRanking5.setObjectName("label_literRanking5")
+        self.verticalLayout_2.addWidget(self.label_literRanking5)
+        self.horizontalLayout_2.addLayout(self.verticalLayout_2)
         MainWindowSplashy.setCentralWidget(self.centralwidget)
 
         self.retranslateUi(MainWindowSplashy)
@@ -104,38 +124,22 @@ class Ui_MainWindowSplashy(object):
     def retranslateUi(self, MainWindowSplashy):
         _translate = QtCore.QCoreApplication.translate
         MainWindowSplashy.setWindowTitle(_translate("MainWindowSplashy", "MainWindow"))
-        item = self.tableRanking.verticalHeaderItem(0)
-        item.setText(_translate("MainWindowSplashy", "1place"))
-        item = self.tableRanking.verticalHeaderItem(1)
-        item.setText(_translate("MainWindowSplashy", "2place"))
-        item = self.tableRanking.verticalHeaderItem(2)
-        item.setText(_translate("MainWindowSplashy", "3place"))
-        item = self.tableRanking.horizontalHeaderItem(0)
-        item.setText(_translate("MainWindowSplashy", "RankingPosition"))
-        item = self.tableRanking.horizontalHeaderItem(1)
-        item.setText(_translate("MainWindowSplashy", "Nickname"))
-        item = self.tableRanking.horizontalHeaderItem(2)
-        item.setText(_translate("MainWindowSplashy", "TotalLiters"))
-        item = self.tableCurrentUser.verticalHeaderItem(0)
-        item.setText(_translate("MainWindowSplashy", "CurrentUser"))
-        item = self.tableCurrentUser.horizontalHeaderItem(0)
-        item.setText(_translate("MainWindowSplashy", "Nickname"))
-        item = self.tableCurrentUser.horizontalHeaderItem(1)
-        item.setText(_translate("MainWindowSplashy", "DailyLiters"))
-        item = self.tableCurrentUser.horizontalHeaderItem(2)
-        item.setText(_translate("MainWindowSplashy", "TotalLiters"))
-        item = self.tableCurrentUser.horizontalHeaderItem(3)
-        item.setText(_translate("MainWindowSplashy", "CurrentPosition"))
-        __sortingEnabled = self.tableCurrentUser.isSortingEnabled()
-        self.tableCurrentUser.setSortingEnabled(False)
-        self.tableCurrentUser.setSortingEnabled(__sortingEnabled)
         self.label.setText(_translate("MainWindowSplashy", "RANKING OF THE WEEK"))
-        self.label_2.setText(_translate("MainWindowSplashy", "SPLASHY STATS"))
-        self.label_3.setText(_translate("MainWindowSplashy", "CURRENT USER STATS"))
-        self.label_4.setText(_translate("MainWindowSplashy", "WEEKLY GOAL"))
+        self.label_3.setText(_translate("MainWindowSplashy", "WELCOME BACK UserNickname !"))
+        self.label_4.setText(_translate("MainWindowSplashy", "MONTHLY GOAL"))
         self.labelSplashyWeeklyGoal.setText(_translate("MainWindowSplashy", "TextLabel"))
-        self.label_6.setText(_translate("MainWindowSplashy", "PLASTIC SAVED:"))
-        self.labelPlasticSaved.setText(_translate("MainWindowSplashy", "TextLabel"))
+        self.label_5.setText(_translate("MainWindowSplashy", "#UserPosition with UserTotalLiters L"))
+        self.labelPlasticSaved.setText(_translate("MainWindowSplashy", "kg of plastic saved"))
+        self.label_nickname1.setText(_translate("MainWindowSplashy", "Name1"))
+        self.label_nickname2.setText(_translate("MainWindowSplashy", "Name1"))
+        self.label_nickname3.setText(_translate("MainWindowSplashy", "Name1"))
+        self.label_nickname4.setText(_translate("MainWindowSplashy", "Name1"))
+        self.label_nickname5.setText(_translate("MainWindowSplashy", "Name1"))
+        self.label_literRanking1.setText(_translate("MainWindowSplashy", "liters"))
+        self.label_literRanking2.setText(_translate("MainWindowSplashy", "liters"))
+        self.label_literRanking3.setText(_translate("MainWindowSplashy", "liters"))
+        self.label_literRanking4.setText(_translate("MainWindowSplashy", "liters"))
+        self.label_literRanking5.setText(_translate("MainWindowSplashy", "liters"))
 
 
     #ADDING FUNCTIONALITIES TO THE GUI
@@ -168,17 +172,20 @@ class Ui_MainWindowSplashy(object):
         plasticSaved = plasticSaved + litersErogated * waterPlasticRatio
 
         userNickname = getFromDB
-        userDailyLiters = getFromDB
         userTotalLiters = getFromDB
         userRankingPosition = getFromDB
 
         firstNickname = getFromDB
         secondNickname = getFromDB
         thirdNickname = getFromDB
+        fourthNickname = getFromDB
+        fifthNickname = getFromDB
 
         firstTotalLiters = getFromDB
         secondTotalLiters = getFromDB
         thirdTotalLiters = getFromDB
+        fourthTotalLiters = getFromDB
+        fifthTotalLiters = getFromDB
 
 
         
@@ -190,27 +197,26 @@ class Ui_MainWindowSplashy(object):
 
         #labels
         self.labelSplashyWeeklyGoal.setText(str(splashyWeeklyGoal) + "L")
-        self.labelPlasticSaved.setText(str(plasticSaved) + "Kg")
+        self.labelPlasticSaved.setText(str(plasticSaved) + "Kg of PLASTIC SAVED")
 
-        #table user
-        self.tableCurrentUser.setItem(0,0,QTableWidgetItem(str(userNickname)))
-        self.tableCurrentUser.setItem(0,1,QTableWidgetItem(str(userDailyLiters)))
-        self.tableCurrentUser.setItem(0,2,QTableWidgetItem(str(userTotalLiters)))
-        self.tableCurrentUser.setItem(0,3,QTableWidgetItem(str(userRankingPosition)))
+        #label user
+        self.label_3.setText("WELCOME BACK " + str(userNickname) + "!")
+        self.label_5.setText("#" + str(userRankingPosition) + " with " + str(userTotalLiters) + "L")
 
-        #table ranking column position
-        self.tableRanking.setItem(0,0,QTableWidgetItem("1st"))
-        self.tableRanking.setItem(1,0,QTableWidgetItem("2nd"))   
-        self.tableRanking.setItem(2,0,QTableWidgetItem("3rd"))
-        #table ranking column nickname
-        self.tableRanking.setItem(0,1,QTableWidgetItem(str(firstNickname)))
-        self.tableRanking.setItem(1,1,QTableWidgetItem(str(secondNickname)))   
-        self.tableRanking.setItem(2,1,QTableWidgetItem(str(thirdNickname)))
-        #table ranking column totalLiters
-        self.tableRanking.setItem(0,2,QTableWidgetItem(str(firstTotalLiters)))
-        self.tableRanking.setItem(1,2,QTableWidgetItem(str(secondTotalLiters)))   
-        self.tableRanking.setItem(2,2,QTableWidgetItem(str(thirdTotalLiters))) 
-
+    
+        #ranking column nickname
+        self.label_nickname1.setText(str(firstNickname))
+        self.label_nickname2.setText(str(secondNickname))
+        self.label_nickname3.setText(str(thirdNickname))
+        self.label_nickname4.setText(str(fourthNickname))
+        self.label_nickname5.setText(str(fifthNickname))
+        #ranking column totalLiters
+        self.label_literRanking1.setText(str(firstTotalLiters))
+        self.label_literRanking2.setText(str(secondTotalLiters))
+        self.label_literRanking3.setText(str(thirdTotalLiters))
+        self.label_literRanking4.setText(str(fourthTotalLiters))
+        self.label_literRanking5.setText(str(fifthTotalLiters))
+        
 
 if __name__ == "__main__":
     import sys
