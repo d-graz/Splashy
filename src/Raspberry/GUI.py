867aa597de0f6dd387d943c04e221c0e94eef59c
Davide Grazzani <davide.grazzani@mail.polimi.it>
2024-05-22 18:36:23 +0200

    Major gui bugfix + added file to delete databse

--- src/Raspberry/GUI.py
+++ src/Raspberry/GUI.py
@@ -22,8 +22,8 @@ class Ui_MainWindowSplashy(object):
         self.nfc = nfc_reader
         MainWindowSplashy.setObjectName("MainWindowSplashy")
         MainWindowSplashy.setEnabled(True)
-        MainWindowSplashy.resize(720, 480)
-        #MainWindowSplashy.showFullScreen()
+        #MainWindowSplashy.resize(720, 480)
+        MainWindowSplashy.showFullScreen()
         MainWindowSplashy.setStyleSheet("background-color: qlineargradient(spread:pad, x1:0, y1:0, x2:0.637368, y2:0.943, stop:0 rgba(127, 163, 216, 255), stop:0.989637 rgba(236, 245, 255, 255))")
         
         self.centralwidget = QtWidgets.QWidget(MainWindowSplashy)
@@ -304,10 +304,10 @@ class Ui_MainWindowSplashy(object):
 
         # read values from database and nfc
         ## get global information
-        splashyMonthlyGoal = self.db.liters_montly_goal * self.db.water_plasic_ratio     #TODO check float                               
-        litersOfTheWeek = self.db.getTotalQuantity() 
+        splashyMonthlyGoal = self.db.liters_montly_goal * self.db.water_plasic_ratio     
+        litersOfTheWeek = self.db.getTotalQuantity() * self.db.water_plasic_ratio
         plasticSaved = self.db.getPlasticSaved()
-        bottleSaved = plasticSaved / 0.015                               #TODO check float
+        bottleSaved = plasticSaved / 0.015
 
         ## get ranking information
         top5 = self.db.getTopK(5)
@@ -339,20 +339,20 @@ class Ui_MainWindowSplashy(object):
         # update text of the labels, tables, ecc with the variables value
         
         #progress bar
-        self.progressBar.setMaximum(splashyMonthlyGoal)  # Imposta il valore massimo a 40
-        self.progressBar.setValue(litersOfTheWeek)    # Imposta il valore corrente (default è 0)
+        self.progressBar.setMaximum(splashyMonthlyGoal)
+        self.progressBar.setValue(litersOfTheWeek)
 
         #labels
-        self.labelMonthlyGoal.setText(str(splashyMonthlyGoal) + " kg")                           
-        self.labelPlasticSaved.setText(str(bottleSaved) + " BOTTLES SAVED !")
+        self.labelMonthlyGoal.setText(self.float_to_string(splashyMonthlyGoal) + " kg of plastic")                           
+        self.labelPlasticSaved.setText(self.float_to_string(bottleSaved) + " BOTTLES SAVED !")
 
         #label user
-        if ranking == -1:
+        if userRankingPosition == -1:
             self.label_3.setText("WELCOME " + str(self.userNickname) + "!")
             self.label_5.setText("")
         else:
             self.label_3.setText("WELCOME BACK " + str(self.userNickname) + "!")
-            self.label_5.setText(str(userRankingPosition) + "°     with " + str(userTotalLiters) + " L")
+            self.label_5.setText(str(userRankingPosition) + "°     with " + self.float_to_string(userTotalLiters) + " L")
 
     
         #ranking column nickname + " L"
@@ -362,11 +362,15 @@ class Ui_MainWindowSplashy(object):
         self.label_nickname4.setText(str(fourthNickname))
         self.label_nickname5.setText(str(fifthNickname))
         #ranking column totalLiters
-        self.label_literRanking1.setText(str(firstTotalLiters) + " L")          #TODO check float
-        self.label_literRanking2.setText(str(secondTotalLiters) + " L")
-        self.label_literRanking3.setText(str(thirdTotalLiters) + " L")
-        self.label_literRanking4.setText(str(fourthTotalLiters) + " L")
-        self.label_literRanking5.setText(str(fifthTotalLiters) + " L")
+        self.label_literRanking1.setText(self.float_to_string(firstTotalLiters) + " L")          
+        self.label_literRanking2.setText(self.float_to_string(secondTotalLiters) + " L")
+        self.label_literRanking3.setText(self.float_to_string(thirdTotalLiters) + " L")
+        self.label_literRanking4.setText(self.float_to_string(fourthTotalLiters) + " L")
+        self.label_literRanking5.setText(self.float_to_string(fifthTotalLiters) + " L")
+
+    def float_to_string(self, value):
+        number = float(value)
+        return "{:.2f}".format(number)
         
 
 
---
+++ src/test/delete_db.py
@@ -0,0 +1,4 @@
+from Raspberry.DatabaseConnectionManager import DatabaseConnectionManager
+
+db = DatabaseConnectionManager()
+db.__deleteTable__()
